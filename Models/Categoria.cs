fa2e00e36b72f875046f00148d9ca2bb7e338d1e
JoanWolf <dasstian@hotmail.com>
2024-10-22 01:49:12 -0500

    Utilización de las Data Annotations Key, ForeignKey, Required, Max Lenght y NotMapped

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -1,10 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace proyectoef.Models;
 
 public class Categoria
 {
+    [Key]
     public Guid Id { get; set; }
-    public string Nombre{ get; set; }
 
+    [Required] // Pone como regla que sea u natributo requerido
+    [MaxLength(150)] // Pone como regla que pueda usar maximo 150 caracteres
+    public string Nombre{ get; set; }
     public string Descripcion { get; set; }
     public virtual ICollection<Tarea> Tareas { get; set; }
 }
--- Models/Tarea.cs
+++ Models/Tarea.cs
@@ -1,13 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace proyectoef.Models;
 
 public class Tarea
 {
-    public Guid Id { get; set; }    
+    [Key]
+    public Guid Id { get; set; }  
+    [ForeignKey("CategoriaId")]  
+
+    [Required]
+    [MaxLength(200)]
     public Guid CategoriaId { get; set; }
+
     public string Descripcion { get; set;}
+
     public Prioridad prioridadTarea { get; set; }
+
     public DateTime FechaCreacion { get; set; }
+
     public virtual Categoria Categoria { get; set; }
+
+    [NotMapped]
+    public string Resumen { get; set; }
 
 }
 
